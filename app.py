e3e9bd6b945bac0e4ec0198ce72d7e1fcc24251c
astershafeer-prog <astershafeer@gmail.com>
2026-04-15 10:08:00 +0400

    fix routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# v13
+# v14
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
